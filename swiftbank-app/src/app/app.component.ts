0fc41f91a18bd9106f25e816e62a65a6fb1e4b86
Ashraf <a.ashrafjfr@gmail.com>
2024-10-08 16:11:50 +0800

    registration page done

--- swiftbank-app/src/app/app.component.ts
+++ swiftbank-app/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginComponent],
+  imports: [RouterOutlet, LoginComponent, RegistrationComponent, RouterModule,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
