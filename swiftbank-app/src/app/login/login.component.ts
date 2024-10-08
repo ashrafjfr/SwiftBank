41bed34f1522b931137e39f518db2c3c37a3c126
Ashraf <a.ashrafjfr@gmail.com>
2024-10-08 12:59:09 +0800

    First commit login component done

---
+++ swiftbank-app/src/app/login/login.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  loginForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      const { username, password } = this.loginForm.value;
+      console.log('Logging in:', username, password);
+      // Implement login logic (API call, etc.)
+    }
+  }
+}
