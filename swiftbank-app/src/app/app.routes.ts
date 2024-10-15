e3b5f7933ee34066578392e960d33cadc4f94681
Ashraf <a.ashrafjfr@gmail.com>
2024-10-15 22:03:49 +0800

    Added transaction and transfer components and routing

--- swiftbank-app/src/app/app.routes.ts
+++ swiftbank-app/src/app/app.routes.ts
@@ -2,11 +2,15 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component';
 import { HomeComponent } from './home/home.component';
+import { TransferComponent } from './transfer/transfer.component';
+import { TransactionsComponent } from './transactions/transactions.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegistrationComponent },
     { path: 'home', component: HomeComponent},
+    { path: 'transactions', component: TransactionsComponent},
+    { path: 'transfer', component: TransferComponent },
     { path: '', redirectTo: '/login', pathMatch: 'full' },
     { path: '**', redirectTo: '/login' }
 ];
