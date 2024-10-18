21127aa8090c09bcfa49ec698435fe39836ee884
zixianlee <96118431+zixianlee@users.noreply.github.com>
2024-10-18 17:09:01 +0800

    Update UserRepository.java

--- swiftbank5/src/main/java/com/example/demo/repository/UserRepository.java
+++ swiftbank5/src/main/java/com/example/demo/repository/UserRepository.java
@@ -3,9 +3,13 @@ package com.example.demo.repository;
 
 
 import com.example.demo.model.User;
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-}
+	 Optional<User> findByUsername(String username);  // Add this method to find a user by username
+	}
