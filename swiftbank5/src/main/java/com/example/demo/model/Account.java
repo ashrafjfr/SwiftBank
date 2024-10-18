ebaad4f62dd23d61e0c4b9584935709b0da8be06
zixianlee <96118431+zixianlee@users.noreply.github.com>
2024-10-18 17:08:27 +0800

    Update Account.java

--- swiftbank5/src/main/java/com/example/demo/model/Account.java
+++ swiftbank5/src/main/java/com/example/demo/model/Account.java
@@ -1,74 +1,64 @@
 package com.example.demo.model;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "accounts")
 public class Account {
 
     @Id
-    //@GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long accountNumber;
 
-    @ManyToOne
+    @OneToOne
     @JoinColumn(name = "user_id", nullable = false)
+    @JsonBackReference
     private User user;
 
-    @Column(nullable = false)
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private AccountType accountType;
-
-  
 
     @Column(nullable = false)
     private double balance;
 
-	public Long getAccountNumber() {
-		return accountNumber;
-	}
+    // Getters and Setters
 
-	public void setAccountNumber(Long accountNumber) {
-		this.accountNumber = accountNumber;
-	}
+    public Long getAccountNumber() {
+        return accountNumber;
+    }
 
-	public User getUser() {
-		return user;
-	}
+    public void setAccountNumber(Long accountNumber) {
+        this.accountNumber = accountNumber;
+    }
 
-	public void setUser(User user) {
-		this.user = user;
-	}
+    public User getUser() {
+        return user;
+    }
 
-	public AccountType getAccountType() {
-		return accountType;
-	}
+    public void setUser(User user) {
+        this.user = user;
+    }
 
-	public void setAccountType(AccountType accountType) {
-		this.accountType = accountType;
-	}
+    public AccountType getAccountType() {
+        return accountType;
+    }
 
-	public double getBalance() {
-		return balance;
-	}
+    public void setAccountType(AccountType accountType) {
+        this.accountType = accountType;
+    }
 
-	public void setBalance(double balance) {
-		this.balance = balance;
-	}
-	
+    public double getBalance() {
+        return balance;
+    }
 
-	public enum AccountType {  // Enum defined inside Transaction class
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public enum AccountType {
         SAVINGS,
         CHECKING
-        
     }
 }
